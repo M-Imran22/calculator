6bef3ffc90fe897372d9d5d3440323388cc31972
M_Imran <muhammadimrann100@gmail.com>
2023-05-19 23:57:21 +0500

    my 1st calculator

---
+++ calculator.js
@@ -0,0 +1,6 @@
+    function clearScreen() {
+      document.getElementById("result").value = "";
+    }
+    function display(value) {
+      document.getElementById("result").value += value;
+    }
